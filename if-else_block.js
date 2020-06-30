7ee21dfa0cfb2f521859f2515246a35bf58295f4
sreeja <62930243+m-sreeja@users.noreply.github.com>
2020-06-30 20:07:15 +0530

    Create if-else_block.js

---
+++ if-else_block.js
@@ -0,0 +1,20 @@
+let grade=10
+let totgrade=(grade<=10)
+console.log(totgrade)
+var a=25
+if(a<10){
+	console.log("average")
+}else if(a<=15){
+	console.log("good")
+}else if(a==25){
+	console.log("outstanding")
+}
+else{
+	console.log("fail")
+}
+var memb="student"
+if(memb == "student"){
+	console.log("Hi!Welcome to the course")
+}else {
+	console.log("Verify your mail!")
+}
